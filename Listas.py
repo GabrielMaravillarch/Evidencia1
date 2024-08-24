6da83210c976782b6a7aea6bed3af20a9f23f058
GabrielMaravillarch <gabriel.maravillarch@uanl.edu.mx>
2024-08-24 07:39:13 -0600

    Create Listas.py

---
+++ Listas.py
@@ -0,0 +1,18 @@
+# Crear una lista de números
+numeros = [1, 2, 3, 4, 5]
+print("Lista de números:", numeros)
+
+# Agregar un elemento a la lista
+numeros.append(6)
+print("Lista después de agregar 6:", numeros)
+
+# Acceder a un elemento por su índice
+print("Elemento en la posición 2:", numeros[2])
+
+# Modificar un elemento en la lista
+numeros[0] = 10
+print("Lista después de modificar el primer elemento:", numeros)
+
+# Eliminar un elemento de la lista
+numeros.remove(3)
+print("Lista después de eliminar el número 3:", numeros)
