29274cf858b0b1a507599adb648b60446f8e410d
GabrielMaravillarch <gabriel.maravillarch@uanl.edu.mx>
2024-08-24 07:50:04 -0600

    Create Evidencia 1.py

---
+++ Evidencia 1.py
@@ -0,0 +1,7 @@
+mi_diccionario = {"nombre": "juan", "edad": 28, "ciudad": "buenos aires"}
+print(mi_diccionario["nombre"])
+print(mi_diccionario["edad"])
+print(mi_diccionario["ciudad"])
+mi_diccionario["profesion"] = "programador"
+mi_diccionario["edad"] = 30
+print(mi_diccionario)
